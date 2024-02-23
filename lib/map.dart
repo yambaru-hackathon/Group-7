344adefbe47727cec39fcd7f0c9703d7799a8f40
Yukinari <yukidora6464@gmail.com>
2024-02-23 10:25:52 +0900

    mapの修正

--- lib/map.dart
+++ lib/map.dart
@@ -4,52 +4,19 @@ import 'package:flutter/material.dart';
 //画面B
 //
 class Map extends StatelessWidget {
-  const Map({Key? key});
+  const Map({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[100],
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 20), // 余白を追加
-            Text(
-              '画面 B',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 20), // 余白を追加
-            Row(
-              children: [
-                Spacer(flex: 1),
-                Container(
-                  padding: EdgeInsets.all(40),
-                  height: 50,
-                  width: 310,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue, width: 5),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: '例 : ギュウカク',
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Spacer(flex: 1),
-              ],
-            ),
-          ],
+      body: const Center(
+        child: Text(
+          '画面 B',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
